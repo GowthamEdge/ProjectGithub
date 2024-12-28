86a8fa57402b07c43265d4c6fc2e36d2c1c0ba25
GowthamEdge <gowtham1999d220@gmail.com>
2024-12-28 13:06:09 +0530

    Methods created in claas A and one Class is Created

--- src/test/java/demo/ClassA.java
+++ src/test/java/demo/ClassA.java
@@ -11,7 +11,17 @@ public class ClassA {
 	private void method3() {
 	System.out.println("3");	
 	}
-	
-	
+	private void india() {
+		System.out.println("IND");
+
+	}
+	private void australia() {
+		System.out.println("AUS");
+
+	}
+	private void pakistan() {
+		System.out.println("PAK");
+
+	}
 	
 }
